1d37dfb28131576c3eb21c2fcd6d61b2c0147c46
szymon <szymonstuszek@gmail.com>
2025-05-01 07:48:41 +0200

    setup securit

--- src/main/java/devs10x/mtg/devs10x_mtg_deckbuilder/controller/CardController.java
+++ src/main/java/devs10x/mtg/devs10x_mtg_deckbuilder/controller/CardController.java
@@ -9,11 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// allow all origins
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/cards")
 public class CardController {
