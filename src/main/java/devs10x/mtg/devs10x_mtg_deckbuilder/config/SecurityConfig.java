3820a195f6ba3e67d27cddf9c34b916eef5fcafa
szymon <szymonstuszek@gmail.com>
2025-05-01 08:10:17 +0200

    note about security toggle

--- src/main/java/devs10x/mtg/devs10x_mtg_deckbuilder/config/SecurityConfig.java
+++ src/main/java/devs10x/mtg/devs10x_mtg_deckbuilder/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package devs10x.mtg.devs10x_mtg_deckbuilder.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,7 +21,10 @@ public class SecurityConfig {
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authorize -> authorize
+                // TODO setup toggle for disabling authentication
+                // .anyRequest().permitAll()
                 .anyRequest().authenticated()
+               
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}))
             .build(); 
